f06c93877f464c3a0eb2d40f8864c24e73831447
tiagoryandev <tiagoryandev@gmail.com>
2023-01-03 12:41:00 -0300

    refactor: use stitches for global styles

--- twitter-react/src/App.tsx
+++ twitter-react/src/App.tsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { globalCss } from "@stitches/react";
+
+const globalStyles = globalCss({
+	"*": {
+		margin: 0,
+		padding: 0,
+		boxSizing: "border-box",
+		fontFamily: "Roboto"
+	}
+});
 
 const App: React.FC = () => {
+	globalStyles();
+
 	return (
 		<h1>Hello, World!</h1>
 	);
--- twitter-react/src/main.tsx
+++ twitter-react/src/main.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import "./styles/globals.css";
-
 const rootElement = document.getElementById("root") as HTMLElement;
 
 ReactDOM.createRoot(rootElement).render(
