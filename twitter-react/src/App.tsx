cfafe2398f1827b4b08f073b4501c7cd7ac6378e
tiagoryandev <tiagoryandev@gmail.com>
2023-01-08 22:54:23 -0300

    revert: reset all website

--- twitter-react/src/App.tsx
+++ twitter-react/src/App.tsx
@@ -1,22 +1,6 @@
-import React from "react";
-import { globalCss } from "@stitches/react";
-
-import Layout from "./components/Layout";
-
-const globalStyles = globalCss({
-	"*": {
-		margin: 0,
-		padding: 0,
-		boxSizing: "border-box",
-		fontFamily: "Roboto"
-	}
-});
-
 const App: React.FC = () => {
-	globalStyles();
-
 	return (
-		<Layout />
+		<h1>Hello, World!</h1>
 	);
 };
 
--- twitter-react/src/components/Layout/styles.ts
+++
@@ -1,7 +0,0 @@
-import { styled } from "@stitches/react";
-
-export const Container = styled("div", {
-	display: "grid",
-	gridTemplateColumns: "600px",
-	height: "100vh"
-});
